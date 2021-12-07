2567e9c4e39194a26727e97be259036e265111aa
leeheeweon <ggoddii@kaoni.com>
2021-12-08 08:42:14 +0900

    짝수 홀수 구하기

---
+++ EvenAndOdd/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public static String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
+}
