742a20c139c091ddd2b98a3444b46bc80f0ade12
leeheeweon <ggoddii@kaoni.com>
2021-12-09 08:54:23 +0900

    자릿수 구해서 더하기

---
+++ AddDigits/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        solution(123);
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        while (n>0) {
+            answer += n%10;
+            n/=10;
+        }
+
+        return answer;
+    }
+}
