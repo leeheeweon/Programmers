01c243b04f8d70171b4db31c1504f1425d5fe674
leeheeweon <ggoddii@naver.com>
2021-12-18 19:57:10 +0900

    문자를 숫자로 변환하기

---
+++ StringChangeToInt/src/com/example/awtSample/Main.java
@@ -0,0 +1,15 @@
+package com.example.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        solution("1234");
+
+    }
+
+    public static int solution(String s) {
+        int num = Integer.parseInt(s);
+        return num;
+    }
+}
