72c97956ef0b66852926e234c189c17fb00fa0dd
leeheeweon <ggoddii@kaoni.com>
2022-08-30 16:46:53 +0900

    ColatzGuess 수정

--- ColatzGuess/src/com/company/Main.java
+++ ColatzGuess/src/com/company/Main.java
@@ -19,8 +19,11 @@ public class Main {
             if (number == 1) {
                 return answer;
             } else {
-                if (number % 2 == 0) number /= 2;
-                else number = number * 3 + 1;
+                if (number % 2 == 0) {
+                    number /= 2;
+                } else{
+                    number = number * 3 + 1;
+                }
             }
         }
         return -1;
