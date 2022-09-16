ca2a435b43ea21934871257d355ea2fbab614018
leeheeweon <ggoddii@kaoni.com>
2022-09-16 09:07:44 +0900

    NumberAndAdditionOfFactors

--- NumberAndAdditionOfFactors/src/com/company/Main.java
+++ NumberAndAdditionOfFactors/src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        solution(13, 17);
+    }
+
+    public static int solution(int left, int right) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i = left; i <= right; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count % 2 == 0) {
+                sum += i;
+            } else {
+                sum -= i;
+            }
+            count = 0;
+        }
+
+        return sum;
     }
 }
