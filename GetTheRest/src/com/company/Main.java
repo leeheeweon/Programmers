1dd9c105e5953338bbe459babbcd215115128525
leeheeweon <ggoddii@kaoni.com>
2022-11-14 08:47:27 +0900

    나머지 구하기

---
+++ GetTheRest/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    static public int solution(int num1, int num2) {
+        int answer = num1%num2;
+        return answer;
+    }
+}
