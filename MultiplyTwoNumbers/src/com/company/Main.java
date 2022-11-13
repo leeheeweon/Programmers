9647d58a9a56569fbc83121ebc0974247db575ce
leeheeweon <ggoddii@kaoni.com>
2022-11-14 08:42:39 +0900

    두수의 곱

---
+++ MultiplyTwoNumbers/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        solution(3,4);
+    }
+
+    static public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
