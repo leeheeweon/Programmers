a9c7f7851cc0a08d70b286e34ae4c5a6a4311f82
leeheeweon <ggoddii@kaoni.com>
2023-07-24 12:00:00 +0900

    AdditionOfFractions

---
+++ AdditionOfFractions/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+    public static int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = {};
+        return answer;
+    }
+}
