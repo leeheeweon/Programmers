1d2b6b861b6d486e2d47f28b7a09bf18fd5f1ec6
leeheeweon <ggoddii@kaoni.com>
2023-07-26 12:00:00 +0900

    FindingTheMode

---
+++ FindingTheMode/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] ints = new int[]{1, 1, 3, 3};
+        System.out.println(solution(ints));
+    }
+
+    public static int solution(int[] array) {
+        List<Map.Entry<Integer, List<Integer>>> list = new ArrayList<>(Arrays.stream(array).boxed().collect(Collectors.groupingBy(o -> o)).entrySet()).stream().sorted((t0, t1) -> Integer.compare(t1.getValue().size(), t0.getValue().size())).collect(Collectors.toList());
+        return list.size() > 1 && list.get(0).getValue().size() - list.get(1).getValue().size() == 0 ? -1 : list.get(0).getKey();
+    }
+}
