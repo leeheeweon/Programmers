fcd4baf8d502f0ed3ba8a92aa38adf264d44d2a6
leeheeweon <ggoddii@kaoni.com>
2023-08-02 14:02:57 +0900

    Print Right Triangle

---
+++ Print Right Triangle/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String star = "*";
+        for (int i = 0; i <n ; i++) {
+            System.out.println(star);
+            star += "*";
+        }
+    }
+}
