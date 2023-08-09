037ecbdc0eda98510e83adfb6137e93f172dd51a
leeheeweon <ggoddii@kaoni.com>
2023-08-09 09:52:01 +0900

    Returning Different Values ​​Depending on Odd or Even

---
+++ Returning Different Values ​​Depending on Odd or Even/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        solution(7);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <=n; i++) {
+            if (n % 2 == 0 && i % 2 == 0) {
+                answer += i*i;
+            } else if (n % 2 != 0 && i % 2 != 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
