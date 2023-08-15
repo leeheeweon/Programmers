bedf713a771248afdfd36407863fc9aca6bd1687
leeheeweon <ggoddii@kaoni.com>
2023-08-15 12:00:00 +0900

    Letter

---
+++ Letter/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        solution("happy birthday!");
+    }
+
+    public static int solution(String message) {
+        return message.length()*2;
+    }
+
+}
