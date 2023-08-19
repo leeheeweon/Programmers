c53cdd89d22e0a5d9ff425812030c1fc4091b42c
leeheeweon <ggoddii@kaoni.com>
2023-08-19 12:00:00 +0900

    String within string

---
+++ String within string/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        solution("ppprrrogrammers", "pppp");
+    }
+
+    public static int solution(String str1, String str2) {
+        int answer = 0;
+
+        if (str1.indexOf(str2) == -1) {
+            answer = 2;
+        } else {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
