27bf1eee558111a210885bb1f3e1948e0795edef
leeheeweon <ggoddii@kaoni.com>
2023-08-22 11:24:00 +0900

    An integer part

---
+++ An integer part/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        solution(1.773);
+    }
+
+    public static int solution(double flo) {
+        return (int) flo;
+    }
+}
