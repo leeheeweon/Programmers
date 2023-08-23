f8372fb1897ccae1fb7c219b2331d5f95bf91439
leeheeweon <ggoddii@kaoni.com>
2023-08-23 13:15:15 +0900

    Up to the nth element

---
+++ Up to the nth element/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        solution(new int[]{5, 2, 1, 7, 5}, 3);
+    }
+
+    public static int[] solution(int[] num_list, int n) {
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
