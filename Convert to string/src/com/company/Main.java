9942a97a7e6bb31f4a5553840946a3542f98d979
leeheeweon <ggoddii@kaoni.com>
2023-08-23 13:41:00 +0900

    Convert to string

---
+++ Convert to string/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public String solution(int n) {
+        return String.valueOf(n);
+    }
+}
