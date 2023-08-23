29c6337b340aed71c2f6a30762be4d6d39d4a4bb
leeheeweon <ggoddii@kaoni.com>
2023-08-23 13:19:49 +0900

    First n characters of string

---
+++ First n characters of string/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        solution("ProgrammerS123", 11);
+    }
+
+    public static String solution(String my_string, int n) {
+        return my_string.substring(0, n);
+    }
+}
