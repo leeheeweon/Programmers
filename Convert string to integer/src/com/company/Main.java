d00bf6f868e908398913147b72d7b3d4955bc678
leeheeweon <ggoddii@kaoni.com>
2023-08-23 13:23:19 +0900

    Convert string to integer

---
+++ Convert string to integer/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        solution("8542");
+    }
+
+    public static int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+
+}
